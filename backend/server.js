eef6b6f4457188b1ad94a80e5a2b9cc8f181067f
Sourabh upadhyay <upadhyaysourabh12@gmail.com>
2024-06-04 12:24:37 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// import path from "path";
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,22 +12,24 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(process.cwd(), "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(process.cwd(), "frontend", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server Running on port ${PORT}`);
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
