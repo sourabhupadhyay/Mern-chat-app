61aa0db3eb776e35d55f9de2ee14c453f84d7fae
Sourabh upadhyay <upadhyaysourabh12@gmail.com>
2024-06-04 17:00:16 +0530

    updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://.onrender.com", {
+			const socket = io("https://we-chat-app-wjpp.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
